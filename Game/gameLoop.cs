122a9f1459ff0dcdfbab45a0e25c6f8ee6f486ac
WilsonSamuel02 <29359529@students.lincoln.ac.uk>
2025-03-11 21:32:46 +0000

    Transferred project files/testing.

---
+++ Game/gameLoop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Media;
+
+namespace DungeonExplorer
+{
+    internal class GameLoop
+    {
+        public void Start()
+        {
+            // Change the playing logic into true and populate the while loop
+            bool running = false;
+            while (playing)
+            {
+                if (gameState == (int)States.START_MENU){
+                    gameState = Menu.StartMenu();
+                }
+                else if (gameState == (int)States.PLAY_GAME){
+                    gameState = Game.Play();
+                }
+                else if (gameState == (int)States.WIN_MENU){
+                    Menu.WinMenu();
+                    running = false;
+                }
+                else if (gameState == (int)States.GAME_OVER){
+                    gameState = Menu.GameOver();
+                }
+                else if (gameState == (int)States.EXIT_MENU){
+                    Menu.ExitMenu();
+                    running = false;
+                }
+                else{
+                    Console.WriteLine("Bye bye!");
+                    Console.WriteLine("Press any key...");
+                    Console.Read();
+                    running = false;
+                }
+            }
+        }
+    }
+}
